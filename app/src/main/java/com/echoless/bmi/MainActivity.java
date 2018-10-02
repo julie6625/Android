d14d8be3f29c858df4ab3e8682f9e80fb694efa4
julie6625 <julie4604@gmail.com>
2018-10-02 16:38:17 +0800

    1.0.3

--- app/src/main/java/com/echoless/bmi/MainActivity.java
+++ app/src/main/java/com/echoless/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.echoless.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void bmi(View view){
+        Log.d( "MainActivity" , "bmi" );    //撰寫log
+        EditText edweight = findViewById(R.id.Ed_weight);   //定位ed_weight
+        EditText edheight = findViewById(R.id.Ed_height);
+        String w = edweight.getText().toString();   //取得weight資料
+        String h = edheight.getText().toString();
+        Log.d("MainActivity" , w+"/ ("+h+"*"+h+")");
+        float weight = Float.parseFloat(w); //字串轉浮點
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height*height);
+        Log.d("MainActivity" , bmi+"");     // msg必要加一個字串 " "
+
+        }
 }
