85bf578fb09c4812992e43d5a5758764857153f1
julie6625 <julie4604@gmail.com>
2018-10-09 16:22:22 +0800

    10/9

--- app/src/main/java/com/echoless/bmi/MainActivity.java
+++ app/src/main/java/com/echoless/bmi/MainActivity.java
@@ -1,23 +1,33 @@
 package com.echoless.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class
+
+MainActivity extends AppCompatActivity {
+
+    private EditText edweight;
+    private EditText edheight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edweight = findViewById(R.id.Ed_weight);//定位ed_weight
+        edheight = findViewById(R.id.Ed_height);
+        //alt+shift+上下移動選取行
+        //ctrl+alt+f 直接宣告field
     }
 
     public void bmi(View view){
         Log.d( "MainActivity" , "bmi" );    //撰寫log
-        EditText edweight = findViewById(R.id.Ed_weight);   //定位ed_weight
-        EditText edheight = findViewById(R.id.Ed_height);
         String w = edweight.getText().toString();   //取得weight資料
         String h = edheight.getText().toString();
         Log.d("MainActivity" , w+"/ ("+h+"*"+h+")");
@@ -25,6 +35,20 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(h);
         float bmi = weight / (height*height);
         Log.d("MainActivity" , bmi+"");     // msg必要加一個字串 " "
+        Toast.makeText(this, "Your BMI :"+ bmi,
+                Toast.LENGTH_LONG).show(); // 顯示給使用者觀看bmi結果(氣球提示)
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is :" + bmi)
+                .setTitle("BMI")
+                .setNegativeButton("OK", null) //negative為左下按鈕positive為右下按鈕 此設定按完OK不做任何行為
+                .setPositiveButton("clear", new DialogInterface.OnClickListener() {        //設定跳出對話框的右下按鈕 顯示clear 並 設定一個onclick的機制 使使用者按下clear之後消除身高體重數值
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edheight.setText("");
+                        edweight.setText("");
+                    }
+                })
+                .show();
 
         }
 }
